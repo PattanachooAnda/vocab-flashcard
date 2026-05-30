95747b9bd02374367d57e0be81ec6de571b609b9
PattanachooAnda <PattanachooAnda@users.noreply.github.com>
2026-05-30 16:48:40 +0000

    deploy: 9c0c2e4ca4ef0666d6d47011eb24feaab5305092

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CFG = {
+  apiKey:            "AIzaSyBcyAeLUmrD_mquNOguvvuzgCLeRunOQhM",
+  authDomain:        "vocab-library-87886.firebaseapp.com",
+  databaseURL:       "https://vocab-library-87886-default-rtdb.firebaseio.com",
+  projectId:         "vocab-library-87886",
+  storageBucket:     "vocab-library-87886.firebasestorage.app",
+  messagingSenderId: "1034193166063",
+  appId:             "1:1034193166063:web:5c7a6023ab6b73894c451a"
+};
